13edd86673f4298368d673b2d33974bcc54f5eef
Tinus Wagner <tinus@3months.com>
2017-01-11 13:23:25 +1300

    test cases for users added

---
+++ db/migrate/20170110220620_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.float :q1
+      t.float :q2
+      t.float :q3
+      t.float :q4
+      t.float :q5
+      t.timestamps
+    end
+  end
+end
