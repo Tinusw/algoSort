842c43a2e65a18c9ce997b88206429c82eee5a34
Tinus Wagner <tinus@3months.com>
2017-01-12 16:25:25 +1300

    documenting percentage method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
     [q1, q2, q3, q4, q5]
   end
 
+  # So now that we've calculated the euclidian distance to get the closeness of two points we need to convert that to a meaningful percentage (how close are the points actually?)
+  # To do this we grab the maximum value a user can attain across questions (e.g. 5 questions with scores out of 100 = 500 or 5/5)
+  # Then we square that value - Math.sqrt (5) = 2.23606797749979
+  # Now we can do (100 - ((euclidean_distance(user)*100)/maximumValue))
+  # We now have a percentage out of a 100 which we can round for fun.
   def distance_to_percentage(other)
     maximumValue = 2.23606797749979
     (100 - ((euclidean_distance(other)*100)/maximumValue)).round
